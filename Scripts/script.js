cd6449c48f62d1631e03829532febe19847f0d32
JeanLLopes <jean.llopes@outlook.com>
2015-06-19 00:06:48 -0300

    Initial Commit

---
+++ Scripts/script.js
@@ -0,0 +1,28 @@
+﻿$(function () {
+    $('#btnProsseguir').on('click', function () {
+        $('#sectionInformacoesPessoais').fadeOut('slow');
+        $('#sectionAcademicos,#sectionMsgSuccessInformacoesPessoais').fadeIn('slow');
+    });
+    $('#btnConcluir').on('click', function () {
+        $('#sectionAcademicos,#sectionMsgSuccessInformacoesPessoais').fadeOut('fast');
+        var aluno = $('#inputNome').val();
+        bootbox.dialog({
+            message: "Cadastro Efetuado com Sucesso",
+            title: "Aluno " + aluno,
+            buttons: {
+                main: {
+                    label: "OK",
+                    className: "btn-primary",
+                    callback: function () {
+                        //LIMPA OS CAMPOS
+                        $('input[type=text').val('');
+                        $('#sectionInformacoesPessoais').fadeIn('fast');
+                        $('#sectionAcademicos,#sectionMsgAcademicos,#sectionMsgSuccessInformacoesPessoais').fadeOut('fast');
+                    }
+
+                },
+            }
+        });
+
+    });
+})
